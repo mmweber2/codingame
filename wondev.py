c868adda8a1d5795ba3e7df84361b1ed4cf5fa16
Mary Ellen Weber <mmweber2@gmail.com>
2017-06-28 10:45:18 -0500

    Initial version for Wood 1 league.
    Does not incorporate pushing.

--- wondev.py
+++ wondev.py
@@ -4,25 +4,37 @@ from collections import namedtuple
 
 Position = namedtuple('Position', 'y x')
 
-
 # TODO: If there is a height 3 area opponent can reach first, build on it
 # TODO: Avoid building on height 2 areas if opponent is adjacent to them
 # TODO: Move back and forth between level 3 spots as much as possible,
 # but build somewhere else
 # TODO: Don't get stuck in a corner by opponent
 # TODO: Don't build on your only move option from a square
+# TODO: Now have two units, choose one of them to move
+# TODO: Try pushing opponent
 
 # Can you build on a square occupied by an opponent?
 
+# TODO: Update to allow pushing
+# Other pos is now a list of Positions
 def is_valid_square(board, move, other_pos):
-    if (-1 in move or size in move or (move == other_pos) or
+    '''Returns True if the square at move is a valid position.
+    
+    A valid position is on the grid and does not contain a hole,
+        level 4 building, or another unit.
+    '''
+    if (-1 in move or size in move or (move in other_pos) or
         board[move.y][move.x] in (".", "4")):
-        # Position is off the grid, a hole, full, or occupied by opponent;
-        #     cannot move or build in this direction
         return False
     return True
-
+    
 def is_valid_move(board, old_pos, new_pos, other_pos):
+    '''Returns True if the change from old_pos to new_pos is valid.
+    
+    new_pos must be a valid square to move or build onto, and the
+        difference in height between old_pos and new_pos must not
+        be greater than 1, but may be negative.
+    '''
     if not is_valid_square(board, new_pos, other_pos):
         return False
     if int(board[old_pos.y][old_pos.x]) + 1 < (int(board[new_pos.y][new_pos.x])):
@@ -37,13 +49,14 @@ def make_move(pos, direction):
         "S": (1, 0), "SW": (1, -1), "W": (0, -1), "NW": (-1, -1)}
     return Position(pos.y + dir_map[direction][0], pos.x + dir_map[direction][1])
 
-# Don't sort the resulting list because we can sort reversed or not depending on use
 def find_adj(board, pos, other_pos):
-    '''Returns a list of all directions with usable squares.
+    '''Returns a sorted list of all directions with usable squares.
+    
     Does not include directions leading to level 4 squares since they are not usable.
-
+    The list is returned in decreasing order of height.
+    
     Return format:
-    [(height, direction), (height, direction) ...]
+    [(height, direction), (height, direction) ...] 
     '''
     directions = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
     results = []
@@ -51,25 +64,35 @@ def find_adj(board, pos, other_pos):
         new_pos = make_move(pos, d)
         if is_valid_square(board, new_pos, other_pos):
             results.append((int(board[new_pos.y][new_pos.x]), d))
-    return results
-
+    return sorted(results, reverse=True)
+    
 # TODO: Don't move onto spots that have no <= 2 spots adjacent, UNLESS it is the end of the game
 # Move to the best spot, then we will build on the best building spot from there
-def find_best_move(board, unit_pos, other_pos):
-    moves = sorted(find_adj(board, unit_pos, other_pos), reverse=True)
+def find_best_move(board, my_units, other_pos):
+    moves = []
+    for i in xrange(len(my_units)):
+        # Count my other unit as an 'other' unit
+        # TODO: Enable pushing
+        other = other_pos.append(my_units[1-i])
+        moves.extend(move + (i,) for move in find_adj(board, my_units[i], other))
     # Choose the highest height square that has building options adjacent to it
-    for height, d in moves:
-        # Don't move to positions with no other adjacent moves
-        if not is_valid_move(board, unit_pos, make_move(unit_pos, d), other_pos):
+    for height, d, unit in moves:
+        unit_pos = my_units[unit]
+        other = other_pos.append(my_units[1-unit])
+        if not is_valid_move(board, unit_pos, make_move(unit_pos, d), other):
             continue
-        if len(find_adj(board, make_move(unit_pos, d), other_pos)) > 1:
-            return d
-
+        if len(find_adj(board, make_move(unit_pos, d), other)) > 1:
+            return (d, unit)
+            
 # Build on a now adjacent spot
+# Other is now a list of other positions
+# Since we already have the unit from when we chose the move, this function
+#     will only search for one of the units.
+# TODO: Can we build on top of our other unit? Currently code allows it.
 def find_best_build(board, unit_pos, other_pos):
-    moves = sorted(find_adj(board, unit_pos, other_pos), reverse=True)
+    moves = find_adj(board, unit_pos, other_pos)
     build_dir = "None"
-    for height, d in moves:
+    for height, d, unit in moves:
         # Try to build on the highest < 3 level square we can, but if our only option
         #    is a level 3 square, build on one of those
         if height < 3:
@@ -81,10 +104,11 @@ def find_best_build(board, unit_pos, other_pos):
         new_pos = make_move(unit_pos, d)
         # Build on any of the level 3 squares (return direction of the first one)
         # The height should be 3 in this case
-        build_dir = moves[0][1] + " " + board[new_pos.y][new_pos.x]
+        assert board[new_pos.y][new_pos.x] == 3
+        build_dir = moves[0][1] + " " + 3
     return build_dir
-
-
+                
+        
 size = int(raw_input())
 units_per_player = int(raw_input())
 
@@ -93,16 +117,20 @@ while True:
     board = []
     for _ in xrange(size):
         board.append(raw_input())
+    my_units = []
+    enemy_units = []
     for _ in xrange(units_per_player):
         unit_x, unit_y = [int(j) for j in raw_input().split()]
-        unit_pos = Position(unit_y, unit_x)
+        my_units.append(Position(unit_y, unit_x))
     for _ in xrange(units_per_player):
         other_x, other_y = [int(j) for j in raw_input().split()]
-        other_pos = Position(other_y, other_x)
+        enemy_units.append(Position(other_y, other_x))
     for _ in xrange(int(raw_input())):
         atype, index, dir_1, dir_2 = raw_input().split()
         index = int(index)
-    move = find_best_move(board, unit_pos, other_pos)
+    # TODO: Find a better way to decide which unit?
+    # TODO: Assign a score to the move for each unit and choose best one
+    move, unit = find_best_move(board, my_units, enemy_units)
     # Position changes after moving; build in best spot from that position
-    build = find_best_build(board, make_move(unit_pos, move), other_pos)
-    print "MOVE&BUILD 0 {} {}".format(move, build)
+    build = find_best_build(board, make_move(my_units[unit], move), enemy_units)
+    print "MOVE&BUILD {} {} {}".format(unit, move, build)
